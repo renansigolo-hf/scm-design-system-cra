e0065cf9111294adb90955392871b3511bffdc62
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2021-12-23 19:14:20 +1100

    fix: regex

--- scripts/check-pr-title.js
+++ scripts/check-pr-title.js
@@ -4,7 +4,7 @@ const github = require("@actions/github");
 async function run() {
   console.log("Running CHECK PR TITLE");
   const regexPattern = new RegExp(
-    /^\[(?<ticket_id>PIKA|KAT|KUS)-\d+\]\s(?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?|^(?<merge>Merge \w+)/
+    /^\[(?<ticket_id>PIKA|KAT|KUS)-\d+\] (?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test): (?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?(?<short_summary>\w) (?<pr_ref>\(#\d+\))$|^(?<merge>Merge \w+)/
   );
   const title = github.context.payload.pull_request?.title;
   if (!regexPattern.test(title)) {
