3bed57ddb3cc11c1b1b2e29a6706ce952bdf00b4
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2021-12-23 16:29:48 +1100

    feat: add scm design system

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,57 @@ import {
   AccordionSummary,
   Alert,
   Button,
+  Card,
+  Divider,
   Skeleton,
-  Typography,
+  Table,
 } from "@hellofresh/scm-design-system";
+import { BoxCancellation } from "@hellofresh/scm-design-system/dist/components/icons";
+import { Typography, TableBody, TableCell, TableRow } from "@material-ui/core";
 import { Link, Route, Routes } from "react-router-dom";
+
+const TableTemplate = () => {
+  const headers = ["Image", "Title", "Date", "Info", "Actions"];
+
+  return (
+    <Table stickyHeader aria-label="sticky table" headers={headers}>
+      <TableBody>
+        <TableRow>
+          <TableCell>
+            {/* <MuiAvatar src="https://picsum.photos/500/300" /> */}
+          </TableCell>
+          <TableCell>Some Title</TableCell>
+          <TableCell>01/08/2021</TableCell>
+          <TableCell>Some info</TableCell>
+          {/* <TableCell>
+            <IconButton variant="secondary" color="primary">
+              <DeleteIcon />
+            </IconButton>
+            <IconButton variant="secondary" color="negative">
+              <SaveIcon />
+            </IconButton>
+          </TableCell> */}
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            {/* <MuiAvatar src="https://picsum.photos/500/300" /> */}
+          </TableCell>
+          <TableCell>Some Title</TableCell>
+          <TableCell>01/08/2021</TableCell>
+          <TableCell>Some info</TableCell>
+          <TableCell>
+            {/* <IconButton variant="secondary" color="primary">
+              <DeleteIcon />
+            </IconButton>
+            <IconButton variant="secondary" color="negative">
+              <SaveIcon />
+            </IconButton> */}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+};
 
 function Home() {
   return (
@@ -62,6 +109,26 @@ function App() {
         onFocusVisible={function noRefCheck() {}}
         variant="primary"
       />
+      <TableTemplate />
+      <Typography color="primary" variant="h1">
+        Heading
+      </Typography>
+
+      <Card>
+        {/* <TopContentExample /> */}
+        <p>asdasd</p>
+        <Divider />
+        {/* <BottomContentExample /> */}
+        <Button
+          color="primary"
+          fullWidth
+          label="Select Card"
+          variant="primary"
+        />
+      </Card>
+
+      <BoxCancellation size="small" state="" />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
