f902cc50c1a58d2256687171d3527e8350113203
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2021-12-23 19:17:17 +1100

    fix

--- scripts/check-pr-title.js
+++ scripts/check-pr-title.js
@@ -4,7 +4,7 @@ const github = require("@actions/github");
 async function run() {
   console.log("Running CHECK PR TITLE");
   const regexPattern = new RegExp(
-    /^\[(?<ticket_id>PIKA|KAT|KUS)-\d+\] (?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test): (?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?(?<short_summary>\w) (?<pr_ref>\(#\d+\))$|^(?<merge>Merge \w+)/
+    /^\[(?<ticket_id>PIKA|KAT|KUS)-\d+\] (?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test): (?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?(?<short_summary>\w)( (?<pr_ref>\(#\d+\)))?$|^(?<merge>Merge \w+)/
   );
   const title = github.context.payload.pull_request?.title;
   if (!regexPattern.test(title)) {
@@ -13,3 +13,5 @@ async function run() {
 }
 
 run();
+
+// (?<pr_ref>\(#\d+\))
