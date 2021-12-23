1f7e98d9ef4b3d457cd2b83022d0995caa4b857f
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2021-12-23 19:50:31 +1100

    patch

--- scripts/check-pr-title.js
+++ scripts/check-pr-title.js
@@ -2,7 +2,6 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  console.log("Running CHECK PR TITLE");
   const regexPattern = new RegExp(
     /^\[(?<ticket_id>PIKA|KAT|KUS)-\d+\] (?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?(?<short_summary>(\w))( (?<pr_ref>\(#\d+\)))?$|^(?<merge>Merge \w+)/
   );
